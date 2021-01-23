9af7f35411611f59981b83ef76ac38af87906a2f
jhunethan <jhunethan2812@gmail.com>
2021-01-23 23:11:33 +0000

    heroku

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 
-// const db = mysql.createPool({
-//   host: "eu-cdbr-west-03.cleardb.net",
-//   user: "b7c8f3e72edffb",
-//   password: "d02605ff",
-//   database: "heroku_a335746522f3d45",
-// });
-
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "layfamilytreedb",
+  host: "eu-cdbr-west-03.cleardb.net",
+  user: "b7c8f3e72edffb",
+  password: "d02605ff",
+  database: "heroku_a335746522f3d45",
 });
+
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "layfamilytreedb",
+// });
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://layfamilytree.netlify.app/"); // update to match the domain you will make the request from
