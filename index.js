1f1a758f3ef9b1303504e02f8705866bf5eb2be5
jhunethan <jhunethan2812@gmail.com>
2021-01-23 23:09:45 +0000

    allow-cors.org

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ const db = mysql.createPool({
   database: "layfamilytreedb",
 });
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://layfamilytree.netlify.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
