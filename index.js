ef6565c837e77f31a0d603502dc6d5efa0fda8ac
jhunethan <jhunethan2812@gmail.com>
2021-01-30 13:16:40 +0000

    cors()

--- index.js
+++ index.js
@@ -18,14 +18,7 @@ const db = mysql.createPool({
   database: "heroku_a335746522f3d45",
 });
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
